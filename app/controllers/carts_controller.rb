85ce9f598c690f95ee38b07b8ff9b42d43eac98e
tabata lisboa <tabatalisboa@pm.me>
2025-05-07 19:28:00 -0300

    ALL OVER AGAIN

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,56 +1,25 @@
 class CartsController < ApplicationController
-  before_action :set_cart
+  # before_action :set_cart
 
   def create
-    product = Product.find(params[:product_id])
-    cart_item = @cart.cart_items.find_by(product: product)
-
-    if cart_item
-      cart_item.update(quantity: cart_item.quantity + params[:quantity].to_i)
-    else
-      @cart.cart_items.create(product: product, quantity: params[:quantity])
-    end
-
-    render json: @cart, serializer: CartSerializerc
+ 
   end
 
   def show
-    render json: @cart, serializer: CartSerializer
   end
 
   def add_item
-    product = Product.find(params[:product_id])
-    item_in_cart = @cart.cart_items.find_by(product: product)
-    
-    if item_in_cart
-      item_in_cart.increase_quantity(amount: params[:quantity]) 
-    else
-      @cart.cart_items.create(product: product, quantity: params[:quantity])
-    end
 
-    render json: @cart, serializer: CartSerializer
   end
 
   def remove_item
-    cart_item = @cart.cart_items.find_by(product_id: params[:product_id])
-
-    if cart_item
-      cart_item.destroy
-      render json: @cart, serializer: CartSerializer
-    else
-      render json: { error: 'Produto não encontrado no carrinho.' }, status: :not_found
-    end
   end
 
   private
   def set_cart
     
-    cart_id = params[:cart_id] || session[:cart_id]
-    @cart = Cart.find_by(id: cart_id)
-  
-    unless @cart
-      @cart = Cart.create
-      session[:cart_id] = @cart.id
-    end
   end
+
+  def cart_params
+  end  
 end
